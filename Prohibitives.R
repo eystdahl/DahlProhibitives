9da27acffc9ef797d48f2dce6064a472a9bcaeb7
eystdahl <93379583+eystdahl@users.noreply.github.com>
2023-10-07 08:44:49 +0200

    Update Prohibitives.R

--- Prohibitives.R
+++ Prohibitives.R
@@ -1,17 +1,69 @@
+#Create matrix of prohibitive clauses in Homeric Greek
+
+GreekProhibitives <- matrix(c(73, 4, 14, 14, 42, 1, 2, 6), ncol = 4, byrow = TRUE)
+
+colnames(GreekProhibitives) <- c("Present imperative", "Aorist imperative", "Aorist subjunctive", "Infinitive")
+rownames(GreekProhibitives) <- c("Iliad", "Odyssey")
+
+#Check
+
+GreekProhibitives
+
+#Perform chi-square test
+
+chisq.test(GreekProhibitives)
+
+#Get CramC)r's V value
+
+library(vcd)
+
+assocstats(GreekProhibitives)
+
+#Make matrix of prohibitive clauses in Early Latin
+
+LatinProhibitives <- matrix(c(45,56,12), ncol = 3, byrow = TRUE)
+
+#Chi-square test
+
+chisq.test(LatinProhibitives)
+
+#Make matrix of Gothic data
+
+GothicProhibitives <- matrix(c(10, 6, 10, 43), ncol = 2, byrow = TRUE)
+colnames(GothicProhibitives) <- c("Imperative", "Optative")
+rownames(GothicProhibitives) <- c("Uncontrollable Events", "Controllable Events")
+GothicProhibitives
+
+chisq.test(GothicProhibitives)
+
+assocstats(GothicProhibitives)
+
+#Create matrix of prohibitive clauses
 
 WALSprohibitives <- matrix(c(113, 182, 55, 146), ncol = 2, byrow = TRUE)
+
+#Add column and row names
 
 colnames(WALSprohibitives) <- c("Same Negation", "Different Negation")
 rownames(WALSprohibitives) <- c("Same form", "Different form")
 
+#Check
 
 WALSprohibitives
 
+#Perform chi-squared test
+
 chisq.test(WALSprohibitives)
+
+#Load vcd package
 
 library(vcd)
 
+#Get CramC)r's V value
+
 assocstats(WALSprohibitives)
+
+#Get expected values
 
 chisq.test(WALSprohibitives)$expected
 
@@ -27,59 +79,9 @@ SameFormDifferentNegation <- matrix(c(182, 113, 146, 55), ncol = 2, byrow = TRUE
 fisher.test(SameFormDifferentNegation, alternative = 'less')
 
 DifferentFormSameNegation <- matrix(c(55, 146, 113, 182), ncol = 2, byrow = TRUE)
+
 fisher.test(DifferentFormSameNegation, alternative = 'less')
 
 DifferentFormDifferentNegation <- matrix(c(146, 55, 182, 113), ncol = 2, byrow = TRUE)
+
 fisher.test(DifferentFormDifferentNegation, alternative = 'greater')
-
-WALSimperatives <- matrix(c(292, 43, 2, 89, 122, 0), ncol = 3, byrow = TRUE)
-
-chisq.test(WALSimperatives)
-
-chisq.test(WALSimperatives)$expected
-
-
-GreekProhibitives <- matrix(c(74, 4, 14, 14, 42, 1, 2, 6), ncol = 4, byrow = TRUE)
-
-colnames(GreekProhibitives) <- c("Present imperative", "Aorist imperative", "Aorist subjunctive", "Infinitive")
-rownames(GreekProhibitives) <- c("Iliad", "odyssey")
-GreekProhibitives
-
-chisq.test(GreekProhibitives)
-
-chisq.test(GreekProhibitives)$expected
-
-LatinProhibitives <- matrix(c(45,56,12), ncol = 3, byrow = TRUE)
-
-chisq.test(LatinProhibitives)
-
-GothicProhibitives <- matrix(c(48,20), ncol = 2, byrow = TRUE)
-
-chisq.test(GothicProhibitives)
-
-IndoEuropeanProhibitives <- matrix(c(3, 4, 2, 5), ncol = 2, byrow = TRUE)
-
-colnames(IndoEuropeanProhibitives) <- c("Same Negation", "Different Negation")
-rownames(IndoEuropeanProhibitives) <- c("Same form", "Different form")
-
-IndoEuropeanProhibitives
-
-IndoEuropeanProhibitivesProportion <- prop.table(IndoEuropeanProhibitives)*100
-
-colnames(IndoEuropeanProhibitivesProportion) <- c("Same Negation", "Different Negation")
-rownames(IndoEuropeanProhibitivesProportion) <- c("Same form", "Different form")
-
-IndoEuropeanProhibitivesProportion
-
-barplot(IndoEuropeanProhibitivesProportion, sub = "Figure 1: Proportion of prohibitive strategies in Indo-European", ylim = c(0,40), beside = TRUE)
-
-legend("topleft", fill = c("black", "grey60"), legend=c("Same form", "Different form"))
-
-WALSprohibitivesProportion <- prop.table(WALSprohibitives)*100
-
-colnames(WALSprohibitivesProportion) <- colnames(WALSprohibitives)
-rownames(WALSprohibitivesProportion) <- colnames(WALSprohibitives)
-WALSprohibitivesProportion
-
-barplot(WALSprohibitivesProportion, sub = "Figure 2: Proportion of prohibitive strategies across languages", ylim = c(0,40), beside = TRUE)
-legend("topleft", fill = c("black", "grey60"), legend=c("Same form", "Different form"))
